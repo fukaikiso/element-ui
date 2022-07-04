55ed6c2dc5f7182b62427f586c03fd65968211c6
fukaikiso <fukaikiso@qq.com>
2022-07-04 22:44:13 +0800

    tabbar

--- src/testing/Navbar.vue
+++ src/testing/Navbar.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
     <mt-navbar v-model="selected">
-      <mt-tab-item id="1">选项一</mt-tab-item>
-      <mt-tab-item id="2">选项二</mt-tab-item>
-      <mt-tab-item id="3">选项三</mt-tab-item>
+      <mt-tab-item id="1"><img src="/img/001.png" alt="" slot="icon" />推荐</mt-tab-item>
+      <mt-tab-item id="2"><img src="/img/004.png" alt="" slot="icon" />科技</mt-tab-item>
+      <mt-tab-item id="3"><img src="@/assets/007.png" alt="" slot="icon" />数码</mt-tab-item>
+      <mt-tab-item id="4"><img src="../assets/025.png" alt="" slot="icon" />军事</mt-tab-item>
     </mt-navbar>
 
     <mt-tab-container v-model="selected">
       <mt-tab-container-item id="1">
-        <mt-cell v-for="n in 10" :key="n" :title="'内容 ' + n" />
+        <mt-cell v-for="n in 10" :key="n" :title="'推荐 ' + n" />
       </mt-tab-container-item>
       <mt-tab-container-item id="2">
-        <mt-cell v-for="n in 4" :key="n" :title="'测试 ' + n" />
+        <mt-cell v-for="n in 4" :key="n" :title="'科技 ' + n" />
       </mt-tab-container-item>
       <mt-tab-container-item id="3">
-        <mt-cell v-for="n in 6" :key="n" :title="'选项 ' + n" />
+        <mt-cell v-for="n in 6" :key="n" :title="'数码 ' + n" />
+      </mt-tab-container-item>
+      <mt-tab-container-item id="4">
+        <mt-cell v-for="n in 6" :key="n" :title="'军事 ' + n" />
       </mt-tab-container-item>
     </mt-tab-container>
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,47 @@
 <template>
   <div>
-    <!-- <mt-header fixed title="关于我们">
-      <router-link to="/" slot="left">
-        <mt-button icon="back">返回</mt-button>
-      </router-link>
-      <mt-button icon="more" slot="right"></mt-button>
-    </mt-header> -->
-    <nav>
-      <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
-      <router-link to="/register">Register</router-link>
-    </nav>
+    <mt-header title="我的主页">
+      <div slot="right" class="header">
+        <router-link to="/login">登录</router-link> |
+        <router-link to="/register">注册</router-link>
+      </div>
+    </mt-header>
+
+    <mt-navbar v-model="selected">
+      <mt-tab-item id="1">UI</mt-tab-item>
+      <mt-tab-item id="2">电商</mt-tab-item>
+      <mt-tab-item id="3">网页</mt-tab-item>
+      <mt-tab-item id="4">交互</mt-tab-item>
+    </mt-navbar>
+
+    <mt-tab-container v-model="selected">
+      <mt-tab-container-item id="1">
+        <mt-cell v-for="n in 10" :key="n" :title="'UI ' + n" />
+      </mt-tab-container-item>
+      <mt-tab-container-item id="2">
+        <mt-cell v-for="n in 4" :key="n" :title="'电商 ' + n" />
+      </mt-tab-container-item>
+      <mt-tab-container-item id="3">
+        <mt-cell v-for="n in 6" :key="n" :title="'网页' + n" />
+      </mt-tab-container-item>
+      <mt-tab-container-item id="4">
+        <mt-cell v-for="n in 6" :key="n" :title="'交互 ' + n" />
+      </mt-tab-container-item>
+    </mt-tab-container>
+
+    <mt-tabbar v-model="tabSelected">
+      <!-- <mt-tab-item id="index"> <img slot="icon" :src="require(`../assets/tabbar/index_${tabSelected == 'index' ? 1 : 0}.png`)" /> 首页 </mt-tab-item> -->
+      <mt-tab-item id="index">
+        <img slot="icon" v-show="tabSelected == 'index'" src="../assets/tabbar/index_1.png" />
+        <img slot="icon" v-show="tabSelected != 'index'" src="../assets/tabbar/index_0.png" />
+        首页
+      </mt-tab-item>
+      <mt-tab-item id="me">
+        <img slot="icon" v-show="tabSelected == 'me'" src="../assets/tabbar/me_1.png" />
+        <img slot="icon" v-show="tabSelected != 'me'" src="../assets/tabbar/me_0.png" />
+        我的
+      </mt-tab-item>
+    </mt-tabbar>
   </div>
 </template>
 
@@ -18,6 +50,12 @@
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
+  data() {
+    return {
+      selected: '1',
+      tabSelected: 'index',
+    };
+  },
   name: 'HomeView',
   components: {
     HelloWorld,
@@ -33,5 +71,11 @@ export default {
 
 a {
   text-decoration: none;
+}
+</style>
+
+<style scoped>
+.header > a {
+  color: #fff;
 }
 </style>
